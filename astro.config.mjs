a8eda0e34af848e0aed9cd309d605b95d112c832
Kevin Alejandro Leal Torres <lealtorreskevin@gmail.com>
2024-10-23 19:56:21 -0600

    Se incluye GitHub Actions para Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  site: 'https://kevinlealtorres.github.io',
 });
